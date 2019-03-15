8a7366f8c11106e4f30389228184cafc5f61e65f
Matt Debont <matt.debont@jncc.gov.uk>
2019-03-15 12:07:28 +0000

    added an error check for createDirectory function

--- workflow/app/workflows/process_s2_swath/common.py
+++ workflow/app/workflows/process_s2_swath/common.py
@@ -5,15 +5,23 @@ import os
 def getFormattedJson(jsonString):
     return json.dumps(jsonString, indent=4)
 
-def makePath(self, newPath):
+def makePath(newPath):
     if not os.path.exists(newPath):
         os.makedirs(newPath)
 
-def clearFolder(self, folder):
+def clearFolder(folder):
     for f in os.listdir(folder):
         path = os.path.join(folder, f)
 
         if os.path.isfile(path):
             os.unlink(path)
         elif os.path.isdir(path):
-            shutil.rmtree(path)    
+            shutil.rmtree(path)
+
+def createDirectory(path):
+    if (os.path.exists(path)):
+        self.clearFolder(path)
+    self.makePath(path)
+
+    if not (os.path.isdir(path)):
+        raise RuntimeError("Could not create the directory: {}".format(path))

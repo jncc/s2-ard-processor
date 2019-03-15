3fb28e751023af5b082f95f1264ff06ea01524f9
Matt Debont <matt.debont@jncc.gov.uk>
2019-03-15 13:58:54 +0000

    Basic structure of the gdal_translate function

--- workflow/app/workflows/process_s2_swath/GdalTranslateKeaToTif.py
+++ workflow/app/workflows/process_s2_swath/GdalTranslateKeaToTif.py
@@ -1,5 +1,6 @@
 import luigi
 import os
+import subprocess
 from luigi import LocalTarget
 from process_s2_swath.CheckFileExists import CheckFileExists
 
@@ -7,12 +8,31 @@ class GdalTranslateKeaToTif(luigi.Task):
     pathRoots = luigi.DictParameter()
     inputFile = luigi.Parameter()
 
+    outputFile = ""
+
     def run(self):
         checkFiletask = CheckFileExists(self.inputFile)
         yield checkFiletask
-        #           gdal_translate -of GTiff -co "COMPRESS=LZW" -co "TILED=YES" -co "BLOCKXSIZE=256" -co "BLOCKYSIZE=256" -co "BIGTIFF=YES" $f ${f%.*}.tif;
-        # generate output file name /file/path/name.tif
+
+        self.outputFile = os.path.splitext(self.inputFile)[0]
+        cmd = "gdal_translate -of GTiff -co \"COMPRESS=LZW\" -co \"TILED=YES\" -co \"BLOCKXSIZE=256\" -co \"BLOCKYSIZE=256\" -co \"BIGTIFF=YES\" {} {}".format(
+            self.inputFile, 
+            self.outputFile
+        )
+
+        command_line_process = subprocess.Popen(
+            cmd,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            shell=True
+        )
+        
+        # TODO: logging probably doesn't work
+        process_output, _ =  command_line_process.communicate()
+        log.info(process_output)
+
+        checkOutputFileTask = CheckFileExists(self.outputFile)
+        yield checkOutputFileTask
 
     def output(self):
-        # outFile =output path/ generated output file name
-        return LocalTarget(outFile)
+        return LocalTarget(self.outputFile)

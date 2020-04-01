b8e39c711d9bd7ae0a938bfdfd5637ae1e7a483c
felix mason <felix.mason@jncc.gov.uk>
2020-04-01 14:21:24 +0100

    added tile id to output report

--- workflow/app/workflows/process_s2_swath/GenerateReport.py
+++ workflow/app/workflows/process_s2_swath/GenerateReport.py
@@ -19,17 +19,18 @@ class GenerateReport(luigi.Task):
     dbFileName = luigi.OptionalParameter(default=None)
 
     def parseInputName(self, productName):
-        pattern = re.compile("S2([AB])_MSIL1C_((20[0-9]{2})([0-9]{2})([0-9]{2})T([0-9]{2})([0-9]{2})([0-9]{2}))_\w+_(R[0-9]{3})")
+        pattern = re.compile("S2([AB])_MSIL1C_((20[0-9]{2})([0-9]{2})([0-9]{2})T([0-9]{2})([0-9]{2})([0-9]{2}))_\w+_(R[0-9]{3})_(T\w+)_")
         
         m = pattern.search(productName)
 
-        satellite = "SENTINEL2%s" % m.group(1)
+        platform = "SENTINEL2%s" % m.group(1)
         captureDate = "%s-%s-%s" % (m.group(3), m.group(4), m.group(5)) 
         captureTime = "%s:%s:%s" % (m.group(6), m.group(7), m.group(8)) 
 
         relativeOrbit = m.group(9)
+        tileNumber = m.group(10)
 
-        return [productName, satellite, relativeOrbit, captureDate, captureTime]
+        return [productName, platform, relativeOrbit, tileNumber, captureDate, captureTime]
 
     def writeToCsv(self, reportLines, reportFilePath):
         exists = os.path.isfile(reportFilePath)
@@ -43,7 +44,7 @@ class GenerateReport(luigi.Task):
             writer = csv.writer(csvFile)
 
             if not exists:  
-                writer.writerow(["ProductId", "Platform", "Relative Orbit", "Capture Date", "Capture Time", "ARD ProductId"])
+                writer.writerow(["ProductId", "Platform", "Relative Orbit", "Tile Number", "Capture Date", "Capture Time", "ARD ProductId"])
 
             for line in reportLines:
                 writer.writerow(line) 
@@ -56,15 +57,15 @@ class GenerateReport(luigi.Task):
 
         if c.fetchone()[0] != 1: 
             c.execute('''CREATE TABLE s2ArdProducts
-                        (productId text, platform text, relativeOrbit text, captureDate text, CaptureTime text, ardProductId text, recordTimestamp text)''')
+                        (productId text, platform text, relativeOrbit text, tileNumber text, captureDate text, captureTime text, ardProductId text, recordTimestamp text)''')
             
             conn.commit()
 
-        sql = "INSERT INTO s2ArdProducts VALUES (?,?,?,?,?,?,?)"
+        sql = "INSERT INTO s2ArdProducts VALUES (?,?,?,?,?,?,?,?)"
 
         for line in reportLines:
             recordTimestamp = str(datetime.now())
-            row = (line[0], line[1], line[2], line[3], line[4], line[5], recordTimestamp)
+            row = (line[0], line[1], line[2], line[3], line[4], line[5], line[6], recordTimestamp)
 
             c.execute(sql, row)
 
